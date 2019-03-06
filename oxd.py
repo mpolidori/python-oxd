466fbf43b36360be0db004ccac76c1cca231b91f
Michael Polidori <michaelapolidori@gmail.com>
2019-03-06 15:54:31 -0500

    More formatting fixes

--- oxd.py
+++ oxd.py
@@ -57,29 +57,37 @@ def definition(word):
 
     for i in range(len(site)):
         if site[i:i + 16] == "class=\"ex\"> <em>":
-            results.append("EX, {}".format(
+            results.append("-EX-{}".format(
                 site[i + 16:i + 16 + site[i + 16:].index("<")]))
 
         if site[i:i + 12] == "class=\"pos\">":
-            results.append("P, {}".format(
+            results.append("-P-{}".format(
                 site[i + 12:i + 12 + site[i + 12:].index("<")]))
 
         if site[i:i + 18] == "class=\"iteration\">":
-            results.append("I, {}".format(
+            results.append("-I-{}".format(
                 site[i + 18:i + 18 + site[i + 18:].index("<")]))
 
         if site[i:i + 26] == "class=\"subsenseIteration\">":
-            results.append("SI, {}".format(
+            results.append("-SI-{}".format(
                 site[i + 26:i + 26 + site[i + 26:].index("<")]))
 
         if site[i:i + 12] == "class=\"ind\">":
             results.append(
-                "--D" + site[i + 12:i + 12 + site[i + 12:].index("<")])
+                "-D-" + site[i + 12:i + 12 + site[i + 12:].index("<")])
+
+        if site[i:i + 22] == "class=\"derivative_of\">" \
+           and site[i + 22] != "<":
+            results.append(
+                "-DO-" + site[i + 22:i + 22 + site[i + 22:].index("<")] +
+                site[i + 22 + site[i + 22:].index(">") + 1:i + 22 +
+                     site[i + 22:].index(">") + 1 + site[i
+                     + 22 + site[i + 22:].index(">") + 1:].index("<")])
 
         if site[i:i + 23] == "class=\"crossReference\">" \
            and site[i + 23] != "<":
             results.append(
-                "CR, " + site[i + 23:i + 23 + site[i + 23:].index("<")] +
+                "-CR-" + site[i + 23:i + 23 + site[i + 23:].index("<")] +
                 site[i + 23 + site[i + 23:].index(">") + 1:i + 23 +
                      site[i + 23:].index(">") + 1 + site[i
                      + 23 + site[i + 23:].index(">") + 1:].index("<")])
@@ -90,9 +98,9 @@ def definition(word):
     if len(results) > 1:
         results = [
             results[i] for i in range(len(results))
-            if results[i] not in "EX, P, I, SI, , --D"
-            and not (results[i][:4] == "EX, "
-                     and results[i - 1][:4] == "EX, ")]
+            if results[i] not in "-EX-P-I-SI-D-DO-"
+            and not (results[i][:4] == "-EX-"
+                     and results[i - 1][:4] == "-EX-")]
 
     last_spaces = 0
 
@@ -115,136 +123,157 @@ def definition(word):
         else:
             second_last = ""
 
-        if "EX, " in item:
+        if item[:4] == "-EX-":
             if results.index(item) < len(results) - 1:
-                if last[:4] == "CR, " \
-                        and results[results.index(item) + 1][:4] == "SI, ":
+                if last[:4] == "-CR-" \
+                        and results[results.index(item) + 1][:4] == "-SI-":
                     continue
 
             item = item.replace("&lsquo;", "'")
             item = item.replace("&rsquo;", "'")
 
-            if second_last[:3] != "I, " and second_last[:4] != "SI, ":
+            if second_last[:3] != "-I-" and second_last[:4] != "-SI-":
                 item = " " + item[4:]
 
             else:
                 item = "  " + item[4:]
 
-        if item[:3] == "P, ":
+        if item[:3] == "-P-":
             print("  {}\n".format(item[3:].upper()))
             continue
 
-        if item[:3] == "I, ":
+        if item[:3] == "-I-":
             continue
 
-        if item[:4] == "SI, ":
+        if item[:4] == "-SI-":
             continue
 
-        if last[:3] == "I, ":
-            if item[:4] == "CR, ":
+        if last[:3] == "-I-":
+            if item[:4] == "-CR-":
                 item = item[4:]
 
             item = last[3:] + "  " + item
 
-        if last[:4] == "SI, ":
-            if item[:4] == "CR, ":
+        if last[:4] == "-SI-":
+            if item[:4] == "-CR-":
                 item = item[4:]
 
             prepend_symbol = "+"
             item = last[4:] + "  " + item
             spaces += 3
 
-        if second_last[:3] == "I, ":
-            if last[:4] == "CR, ":
+        if second_last[:3] == "-I-":
+            if last[:4] == "-CR-":
                 item = "   " + item
 
             else:
                 spaces += 2
 
-        if second_last[:4] == "SI, ":
+        if second_last[:4] == "-SI-":
             if len(second_last[4:]) > 3:
                 v = 8
 
             else:
                 v = 7
 
-            if last[:4] == "CR, ":
+            if last[:4] == "-CR-":
                 item = " " * v + item
 
             spaces += v
 
-        if item[:4] == "CR, ":
-            if results.index(item) < len(results) - 1:
-                if results[results.index(item) + 2][:4] == "SI, ":
-                    spaces += 2
-
-            if last[:3] == "--D":
-                if second_last[:3] == "I, ":
+        if item[:4] == "-CR-" and item != results[-2]:
+            if last[:3] == "-D-":
+                if second_last[:3] == "-I-":
                     spaces += len(second_last[3:]) + 1
 
-                if second_last[:4] == "SI, ":
+                elif second_last[:4] == "-SI-":
                     spaces += len(second_last[4:]) - 1
+
+                else:
+                    spaces += 1
 
             else:
                 spaces += 2
 
             item = item[4:]
 
-        if (item[spaces] == "'" or item.count("'") == 2) and item[-1] == "'":
-            if last[:4] == "CR, ":
-                spaces = last_spaces - 3
+        if (item[spaces] == "'" or item.count("'") >= 2) and item[-1] == "'":
+            if last[:4] == "-CR-":
+                if second_last[:4] == "-EX-":
+                    spaces = last_spaces - 1
 
-            if second_last[:3] == "--D":
+                else:
+                    spaces = last_spaces - 3
+
+            if second_last[:3] == "-D-":
                 spaces = last_spaces - 1
 
-            if last[:3] == "I, " or last[:4] == "SI, ":
+            if last[:3] == "-I-" or last[:4] == "-SI-":
                 item = item[:item.index("'") - 1] + item[item.index("'"):]
+
+        if last[:4] == "-CR-" and last == results[-2]:
+            spaces += 1
 
         item = " " * spaces + item
         last_spaces = spaces
 
-        if last[:3] == "I, " or last[:4] == "SI, ":
+        if item[spaces:spaces + 4] == "-CR-":
+            item = prepend_symbol + item[:spaces] + item[spaces + 4:]
+
+        if item[spaces:spaces + 4] == "-DO-":
+            item = prepend_symbol + item.replace("-DO-", "")
+
+        if last[:3] == "-I-" or last[:4] == "-SI-":
             item = prepend_symbol + item
 
-        if (last[:4] == "EX, " and second_last[:3] == "--D"
-                and results[results.index(last) + 3][:4] != "SI, "
-                or last[:3] == "P, " and item[2:] in results[1]
-                or len(results) == 1) and "--D" not in item:
+        if last[:3] == "-P-" and item[spaces:] == results[-1][4:]:
             item = prepend_symbol + item[2:]
 
-        if "--D" in item:
-            if last[:3] != "I, " and last[:4] != "SI, ":
+        if (last[:4] == "-EX-" and second_last[:3] == "-D-"
+                and results.index(last) < len(results) - 3
+                and results[results.index(last) + 3][:4] != "-SI-"
+                or len(results) == 1) and "-D-" not in item:
+            item = prepend_symbol + item[2:]
+
+        if item[3:] in results[-1] and last[:3] == "-P-":
+            if last == results[0]:
+                item == prepend_symbol + item[4:]
+            else:
+                item = prepend_symbol + item[2:]
+
+        if "-D-" in item:
+            if last[:3] != "-I-" and last[:4] != "-SI-":
                 item = prepend_symbol + " " + item[spaces + 3:]
 
             else:
-                item = item[:item.index("--D")] + item[item.index("--D") + 3:]
+                item = item[:item.index("-D-")] + item[item.index("-D-") + 3:]
 
         if "&#39;" in item:
             item = item.replace("&#39;", "'")
 
         if len(item) + 4 > term_width:
-            if second_last[:3] == "I, ":
-                if last[:4] == "CR, ":
+            if second_last[:3] == "-I-":
+                if last[:4] == "-CR-":
                     spaces += 7
 
                 else:
                     spaces += 1
 
-            if second_last[:4] == "SI, ":
-                if last[:4] == "CR, ":
+            if second_last[:4] == "-SI-":
+                if last[:4] == "-CR-":
                     spaces += 8
 
                 else:
                     spaces += 1
 
-            if last[:3] == "I, ":
+            if last[:3] == "-I-":
                 spaces += len(last[3:]) + 3
 
-            if last[:4] == "SI, ":
+            if last[:4] == "-SI-":
                 spaces += len(last[4:]) + 3
 
             if item[spaces + 1] != "'" and item[-1] != "'":
-                if last[:3] != "I, " and last[:4] != "SI, ":
+                if last[:3] != "-I-" and last[:4] != "-SI-":
                     spaces += 1
 
             else:
